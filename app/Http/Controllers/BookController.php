f9ab793e1e7654247e4f63d77eec483c5d75cafd
laevian <lpocklin@gmail.com>
2016-03-09 18:42:20 -0500

    Demonstration of environment configurations

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Foobooks\Http\Controllers;
+
+use Foobooks\Http\Controllers\Controller;
+
+class BookController extends Controller {
+
+    /**
+    * Responds to requests to GET /books
+    */
+    public function getIndex() {
+          return "Shows a list of all books";
+    }
+
+    /**
+     * Responds to requests to GET /books/show/{id}
+     */
+    public function getShow($id) {
+        return 'Show book: '.$id;
+    }
+
+    /**
+     * Responds to requests to GET /books/create
+     */
+    public function getCreate() {
+      $view = '<form method="POST" action="/book/create">';
+      $view .= csrf_field();
+      $view .= 'Book title: <input type="text" name="title">';
+      $view .= '<input type="submit">';
+      $view .= '</form>';
+
+      return $view;
+    }
+
+    /**
+     * Responds to requests to POST /books/create
+     */
+    public function postCreate() {
+    return 'Add the book: '.$_POST['title'];
+    }
+}
