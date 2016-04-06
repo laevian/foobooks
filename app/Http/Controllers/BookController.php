b1d34bb6141e15f643dab894efbc22b694065fbc
laevian <lpocklin@gmail.com>
2016-04-06 19:50:23 -0400

    updating project routes

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -16,8 +16,9 @@ class BookController extends Controller {
     /**
      * Responds to requests to GET /books/show/{id}
      */
-    public function getShow($id) {
-        return 'Show book: '.$id;
+    public function getShow($title = null) {
+        return view("books.show")->with('title', $title);
+        #return 'Show book: '.$title;
     }
 
     /**
